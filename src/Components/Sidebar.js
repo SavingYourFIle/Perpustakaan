b7dac78c8afaba8c282f7dfa7771d77f819d3e61
SavingYourFIle <80606605+SavingYourFIle@users.noreply.github.com>
2023-05-07 17:44:53 +0700

    tes

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,33 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Sidebar.css'
 import { SidebarData } from './SidebarData.js';
+import * as IoIcons from 'react-icons/io';
 import Button from './Button.js';
 
 function Sidebar(props) {
     
     return (
         <>
+        
             <nav className="sidebar">
+
+                <p className='title'> <IoIcons.IoIosBook/>  <span>Perpustakaangit </span> </p>
+                <hr></hr>
                 <ul className='menu-items'>
-                    <p className='title'>
-                            Perpustakaan
-                    </p>
-                    
                     {SidebarData.map((item,index) =>{
                         return (
                             <Button icon={item.icon} path={item.path} title={item.title}  />
-
-                            /*<li key={index} className={item.cName}>
-                                <Link to={item.path}>
-                                {item.icon}
-                                <span>{item.title}</span>
-                                </Link>
-                            </li>*/
                         );
                     })}
+                    
                 </ul>
-
+                <hr></hr>
             </nav>
+            
         </>
     );
 }
